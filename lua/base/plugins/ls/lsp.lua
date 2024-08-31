6844b560d4bcdefc82645e94a45d66f409c28ba3
8bitbug <akrammohadin@gmail.com>
2024-09-01 00:01:10 +0200

    Optimised lsp

--- lua/base/plugins/ls/lsp.lua
+++ lua/base/plugins/ls/lsp.lua
@@ -1,5 +1,5 @@
 local lspconfig = require('lspconfig')
-local mason = require('mason-lspconfig');
+local mason = require('mason-lspconfig')
 
 require('mason').setup()
 
@@ -8,26 +8,48 @@ mason.setup({
 })
 
 local on_attach = function(_, _)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
-lspconfig.clangd.setup{
-    on_attach = on_attach,
-}
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-lspconfig.lua_ls.setup{
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = {'vim'},
+mason.setup_handlers({
+    function(server_name)
+        lspconfig[server_name].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            settings = {}
+        })
+    end,
+
+    ["lua_ls"] = function()
+        lspconfig.lua_ls.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                },
             },
-        },
-    },
-}
+        })
+    end,
+})
+
+-- Setup for system-installed LSP (clangd)
+lspconfig.clangd.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "clangd" },
+})
