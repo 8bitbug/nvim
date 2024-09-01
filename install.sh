e0a486b16a72fd05c014f02adc9ff3ab1871d9c4
8bitbug <akrammohadin@gmail.com>
2024-09-01 14:36:16 +0200

    Added bash

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install -y cmake ninja-build gettext unzip curl git file
+
+if git clone https://github.com/neovim/neovim.git; then
+    cd neovim
+    make CMAKE_BUILD_TYPE=RelWithDebInfo
+    ls
+    cd build
+    cpack -G Deb
+    sudo dpkg -i --force-overwrite nvim-linux64.deb
+    echo "I: Installed neovim"
+else
+    echo "E: Unable to clone repo"
+fi
