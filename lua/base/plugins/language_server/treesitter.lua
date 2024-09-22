b6071de957c34333921850e00d52d77fe3e677f8
8bitbug <akrammohadin@gmail.com>
2024-09-22 13:08:44 +0200

    Tressitter config update

--- lua/base/plugins/language_server/treesitter.lua
+++ lua/base/plugins/language_server/treesitter.lua
@@ -1,4 +1,7 @@
 require('nvim-treesitter.configs').setup({
-    highlight = {enable = true, additional_vim_regex_highlighting = false},
-    indent = {enable = true}
+  ensure_installed = { "lua", "c", "cpp" },
+  sync_install = true,
+  auto_install = true,
+  highlight = { enable = true, additional_vim_regex_highlighting = false },
+  indent = { enable = true }
 })
