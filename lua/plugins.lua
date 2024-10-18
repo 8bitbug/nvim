32bb5c4fc32d0e044f7c7eeebb716a21146fd532
8bitbug <akrammohadin@gmail.com>
2024-10-18 23:48:00 +0200

    Changed a bit about the theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -83,6 +83,9 @@ require("lazy").setup({
     "sainnhe/gruvbox-material",
     priority = 1000,
     config = function()
+      vim.o.background = "dark"
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_better_performance = 1
       vim.cmd([[colorscheme gruvbox-material]])
     end,
   },
