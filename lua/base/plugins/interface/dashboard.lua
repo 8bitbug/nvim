7faf374449be68d7742117912e680ea028218a88
8bitbug <akrammohadin@gmail.com>
2024-10-18 22:03:10 +0200

    Added nerd font icons to dashboard(alpha.nvim)

--- lua/base/plugins/interface/dashboard.lua
+++ lua/base/plugins/interface/dashboard.lua
@@ -11,11 +11,11 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
-  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-  dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
-  dashboard.button("s", "  Settings", ":e $MYVIMRC <CR>"),
-  dashboard.button("q", "  Quit NVIM", ":qa<CR>")
+  dashboard.button("e", "    New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("f", "  󰈞  Find file", ":Telescope find_files <CR>"),
+  dashboard.button("r", "  󱋡  Recent files", ":Telescope oldfiles <CR>"),
+  dashboard.button("s", "    Settings", ":e $MYVIMRC <CR>"),
+  dashboard.button("q", "    Quit NVIM", ":qa<CR>")
 }
 
 local function footer()
